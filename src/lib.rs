9d4fcb2b1b934783e65037a6379d2783cc8cae80
xiaobin <xiaobinh@hotmail.com>
2021-05-19 16:17:43 +0800

    json config supported

--- src/lib.rs
+++ src/lib.rs
@@ -6,38 +6,34 @@ use actix_web::{
     App, HttpResponse, HttpServer,
 };
 use env_logger;
-use tool::{app_state::AppState, token::TokenTool};
+use tool::{app_state::AppState, configure::Config, token::TokenTool};
 
 mod app;
 pub mod middlewares;
 pub mod tool;
 
 pub async fn start() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let config = Data::new(Config::from_file("./config.json"));
+    let port = config.port();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or(config.log_level()));
     let app_state = Data::new(AppState::new("My Web"));
-    let token_tool = Data::new(TokenTool::new(b"d566cb10-aa55-11eb-bcbc-0242ac130002"));
+    let token_tool = Data::new(TokenTool::new(config.key()));
     HttpServer::new(move || {
         App::new()
-            // .wrap(app::middleware::sayhi::SayHi)
-            // .wrap(
-            //     CookieSession::signed(&key)
-            //         .secure(false)
-            //         .http_only(true)
-            //         .expires_in(1800),
-            // )
             .wrap(Logger::default())
             .wrap(Compress::default())
-            .app_data(app_state.clone())
+            .app_data(config.clone())
             .app_data(token_tool.clone())
+            .app_data(app_state.clone())
             .configure(app::config)
-            .service(Files::new("/", "./static").show_files_listing())
+            .service(Files::new("/", config.static_dir()).show_files_listing())
             .default_service(
                 web::route()
                     .guard(guard::Not(guard::Get()))
                     .to(|| HttpResponse::NotFound().body("404 Not Found!")),
             )
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format!("127.0.0.1:{}", port))?
     .run()
     .await
 }
--- src/tool/configure.rs
+++ src/tool/configure.rs
@@ -1,1 +1,46 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config {
+    port: i32,
+    key: String,
+    static_dir: Option<String>,
+    log_level: Option<String>,
+}
+
+impl Config {
+    pub fn from_file(path: &str) -> Config {
+        let content = fs::read_to_string(path).unwrap();
+        let config: Config = serde_json::from_str(&content).unwrap();
+        config
+    }
+
+    pub fn port(&self) -> i32 {
+        self.port
+    }
+
+    pub fn key(&self) -> &[u8] {
+        self.key.as_bytes()
+    }
+
+    pub fn static_dir(&self) -> &str {
+        match &self.static_dir {
+            Some(static_dir) => static_dir.as_str(),
+            None => "./static",
+        }
+    }
+
+    pub fn log_level(&self) -> &str {
+        match &self.log_level {
+            Some(log_level) => log_level.as_str(),
+            None => "info",
+        }
+    }
+}
+
+#[test]
+fn tet() {
+    let config = Config::from_file("./config.json");
+    println!("{:?}", config);
+}
